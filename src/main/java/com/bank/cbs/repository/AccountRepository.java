bfc50e11b59f140068cda447c600718d7d283726
nikhilvishwakarma077 <nikhilvishwakarma7077@gmail.com>
2026-06-08 23:21:39 +0530

    java file deleted

--- src/main/java/com/bank/cbs/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.cbs.repository;
-
-import com.bank.cbs.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
--- src/main/java/com/bank/cbs/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.cbs.repository;
-
-import com.bank.cbs.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
